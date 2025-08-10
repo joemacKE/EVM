d7b3a30e7080a8789aad5a79987992cd8a0588dd
joemacKE <josephmuga2020@gmail.com>
2025-08-10 15:52:45 +0300

    Imported the TokenObtainPairView,     TokenRefreshView, TokenBlacklistView to my main urls file. I intend to authenticate users using drf-simplejwt and blacklist the tokens once users has logged out.

--- EMS/urls.py
+++ EMS/urls.py
@@ -1,6 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +13,8 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/', include('events.api.urls')),
     path('api/', include('accounts.api.urls')),
+
+    # JWT Authentication URLs
+    path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    
 ]
